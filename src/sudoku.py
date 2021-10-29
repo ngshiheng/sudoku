3aae32b11fa8f9b045c468477e01b8641b268b38
Jerry Ng <ngshiheng@gmail.com>
2021-10-29 11:07:31 +0800

    Add more test cases

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,22 +1,11 @@
 from typing import List
 
-EXAMPLE_BOARD = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9],
-]
-
 
 class Sudoku:
     """A Sudoku object with custom methods"""
+    BLANK_CELL = 0
     BOARD_SIZE = 9
-    SUB_BOXES_SIZE = 9 // 3
+    SUB_BOXES_SIZE = BOARD_SIZE // 3
 
     def __init__(self, board) -> None:
         self.board = board
@@ -25,17 +14,21 @@ class Sudoku:
         self.box_check = [[set() for _ in range(self.SUB_BOXES_SIZE)] for _ in range(self.SUB_BOXES_SIZE)]
 
     def print_board(self) -> None:
-        """Prints a Sudoku board"""
+        """Prints the current state of the Sudoku board"""
         print("-" * self.BOARD_SIZE * 3)
         for i in range(self.BOARD_SIZE):
             print(self.board[i])
 
-    def validate(self) -> bool:
+    def generate_board(self) -> None:
+        """Generates a random valid Sudoku board"""
+        raise NotImplementedError()
+
+    def validate_board(self) -> bool:
         """Validates a Sudoku board"""
         for i in range(self.BOARD_SIZE):
             for j in range(self.BOARD_SIZE):
                 number = self.board[i][j]
-                if number == 0:
+                if number == self.BLANK_CELL:
                     continue
 
                 if number in self.row_check[i]:
@@ -52,22 +45,21 @@ class Sudoku:
 
         return True
 
-    def solve(self) -> List[List[int]]:
+    def solve_board(self) -> List[List[int]]:
         """Solves a valid Sudoku board"""
-        if self.validate():
-            for i in range(self.BOARD_SIZE):
-                for j in range(self.BOARD_SIZE):
-                    if self.board[i][j] == 0:
-                        self._backtrack(i, j)
-            return self.board
+        if not self.validate_board():
+            raise ValueError("Invalid board")
 
-        raise ValueError("Invalid board")
+        for i in range(self.BOARD_SIZE):
+            for j in range(self.BOARD_SIZE):
+                if self.board[i][j] == self.BLANK_CELL:
+                    self._backtrack(i, j)
+
+        return self.board
 
     def _backtrack(self, i: int, j: int) -> bool:
-        """A backtracking helper function for self.solve()"""
-        self.print_board()
-
-        if self.board[i][j] != 0:
+        """A backtracking helper function to help solve the Sudoku"""
+        if self.board[i][j] != self.BLANK_CELL:
             j += 1
             if j == self.BOARD_SIZE:
                 i += 1
@@ -88,7 +80,7 @@ class Sudoku:
                 if self._backtrack(i, j):
                     return True
 
-                self.board[i][j] = 0
+                self.board[i][j] = self.BLANK_CELL
                 self.row_check[i].remove(number)
                 self.col_check[j].remove(number)
                 self.box_check[i // 3][j // 3].remove(number)
@@ -97,6 +89,17 @@ class Sudoku:
 
 
 if __name__ == "__main__":
-    sudoku = Sudoku(EXAMPLE_BOARD)
-    sudoku.solve()
-    sudoku.print_board()
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ]
+
+    sudoku = Sudoku(board)
+    sudoku.solve_board()
